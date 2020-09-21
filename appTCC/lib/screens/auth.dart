915e35fb41f31aed7e07bf788f17cfd473f0a14d
BrianHM <messiasbrian@gmail.com>
2020-09-21 22:02:06 +0000

    Criação estrutura básica do app

---
+++ appTCC/lib/screens/auth.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance; //"_ indica que é privado"
+
+  //sign in anon
+
+  //sign in com email e senha
+
+  //registrar com email e senha
+
+  //sing out
+}
---
+++ appTCC/lib/screens/wrapper.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:appTCC/screens/home/home.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //return Home ou Autheticate Widget
+    return Home();
+  }
+}
